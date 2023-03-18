f3d3bc6c9e75282aaec51c63807c2aed91905a34
KirBotz <92134398+KirBotz@users.noreply.github.com>
2023-03-18 21:24:41 +0700

    Add files via upload

---
+++ kw.js
@@ -0,0 +1,61 @@
+const { baileys, proto, generateWAMessageFromContent, getContentType } = require("@adiwajshing/baileys")
+const { getGroupAdmins, fetchJson } = require("./functions.js")
+const { exec } = require("child_process")
+const cheerio = require("cheerio")
+const chalk = require("chalk")
+const util = require("util")
+const axios = require("axios")
+const fs = require("fs")
+const nomorOwner = ['6287705048235']
+
+module.exports = async (client, mek, msg) => {
+try {
+const type = getContentType(msg.message)
+const content = JSON.stringify(msg.message)
+const from = msg.key.remoteJid
+const quoted = type == 'extendedTextMessage' && msg.message.extendedTextMessage.contextInfo != null ? msg.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
+const body = (type === 'conversation' && msg.message.conversation) ? msg.message.conversation : (type == 'imageMessage') && msg.message.imageMessage.caption ? msg.message.imageMessage.caption : (type == 'documentMessage') && msg.message.documentMessage.caption ? msg.message.documentMessage.caption : (type == 'videoMessage') && msg.message.videoMessage.caption ? msg.message.videoMessage.caption : (type == 'extendedTextMessage') && msg.message.extendedTextMessage.text ? msg.message.extendedTextMessage.text : (type == 'buttonsResponseMessage' && msg.message.buttonsResponseMessage.selectedButtonId) ? msg.message.buttonsResponseMessage.selectedButtonId : (type == 'templateButtonReplyMessage') && msg.message.templateButtonReplyMessage.selectedId ? msg.message.templateButtonReplyMessage.selectedId : ''
+const prefix = /^[°zZ#$@*+,.?=''():√%!¢£¥€π¤ΠΦ_&><`™©®Δ^βα~¦|/\\©^]/.test(body) ? body.match(/^[°zZ#$@*+,.?=''():√%¢£¥€π¤ΠΦ_&><!`™©®Δ^βα~¦|/\\©^]/gi) : '.'
+const isCmd = body.startsWith(prefix)
+const command = isCmd ? body.slice(prefix.length).trim().split(' ').shift().toLowerCase() : ''
+const args = body.trim().split(/ +/).slice(1)
+const isGroup = from.endsWith('@g.us')
+const botNumber = client.user.id.split(':')[0]
+const sender = msg.key.fromMe ? (client.user.id.split(':')[0]+'@s.whatsapp.net' || client.user.id) : (msg.key.participant || msg.key.remoteJid)
+const senderNumber = sender.split('@')[0]
+const pushname = msg.pushName || `${senderNumber}`
+const isBot = botNumber.includes(senderNumber)
+const isOwner = nomorOwner.includes(senderNumber) || isBot
+
+if (isCmd && msg.isGroup) { 
+console.log(chalk.bold.rgb(255, 178, 102)('\x1b[1;31m~\x1b[1;37m> [\x1b[1;32mCMD\x1b[1;37m]'), chalk.bold.rgb(153, 255, 153)(command), chalk.bold.rgb(204, 204, 0)("from"), chalk.bold.rgb(153, 255, 204)(pushname), chalk.bold.rgb(204, 204, 0)("in"), chalk.bold.rgb(255, 178, 102)("Group Chat"), chalk.bold('[' + args.length + ']')); 
+}
+if (isCmd && !msg.isGroup) { 
+console.log(chalk.bold.rgb(255, 178, 102)('\x1b[1;31m~\x1b[1;37m> [\x1b[1;32mCMD\x1b[1;37m]'), chalk.bold.rgb(153, 255, 153)(command), chalk.bold.rgb(204, 204, 0)("from"), chalk.bold.rgb(153, 255, 204)(pushname), chalk.bold.rgb(204, 204, 0)("in"), chalk.bold.rgb(255, 178, 102)("Private Chat"), chalk.bold('[' + args.length + ']')); 
+}
+
+if (command) { 
+await client.readMessages([msg.key]) 
+}
+
+switch (command) {
+case "tes":{
+client.sendMessage(from, { text : "Iya Kak?" }, { quoted: msg })
+}
+break
+default:
+}
+} catch (err) {
+console.log(util.format(err))
+let e = String(err)
+client.sendMessage("6287705048235@s.whatsapp.net", {text:e})
+}
+}
+
+let file = require.resolve(__filename) 
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update ${__filename}`))
+delete require.cache[file]
+require(file)
+})
